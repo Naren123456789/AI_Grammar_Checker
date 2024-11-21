55fcf218cdf9ebe295dacdbf2dad53a54682703b
Naren123456789 <stbnarenkishor@gmail.com>
2024-11-21 11:55:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 //const genAI = new GoogleGenerativeAI("AIzaSyCBxFpDI3sfqPKguQ9NkVuEyFQwiygDU3I");
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('public', path.join(__dirname, 'public'));
+//app.set('public', path.join(__dirname, 'public'));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public", path.join(__dirname, 'public')));
+//app.use(express.static("public", path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
     res.render("index", {
