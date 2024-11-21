ed56dccdbe1090598a1d4b4cedde855c486dfb1e
Naren123456789 <stbnarenkishor@gmail.com>
2024-11-21 12:39:24 +0530

    favicon update

--- app.js
+++ app.js
@@ -9,10 +9,9 @@ const port = process.env.PORT || 5000;
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 app.set('views', path.join(__dirname, 'views'));
-//app.set('public', path.join(__dirname, 'public'));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-//app.use(express.static("public", path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
     res.render("index", {
